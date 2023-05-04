81e61c990409dfcdca29586a685ea6e00b35fc39
Bravo1B9 <brofe93@gmail.com>
2023-05-04 09:55:04 +0000

    Initial interaction from UI

--- src/App.js
+++ src/App.js
@@ -1,4 +1,24 @@
 function App() {
+
+  const getMessages = async () => {
+    const options = {
+      method:'POST',
+      body: JSON.stringify({
+        message: 'Hello, how are you?'
+      }),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }
+    try {
+      const response = await fetch('https://8000-brendonrofe-reactchatgp-b3b91exod4b.ws-eu96b.gitpod.io/completions', options);
+      const data = await response.json();
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div className="app">
       <section className="side-bar">
@@ -18,7 +38,7 @@ function App() {
         <div className="bottom-section">
           <div className="input-container">
             <input/>
-            <div id="submit">➢</div>
+            <div id="submit" onClick={getMessages}>➢</div>
           </div>
           <p className="info">BrendonGPT may produce inaccurate information about people, places, or facts. BrendonGPT Mar 23 Version</p>
         </div>
