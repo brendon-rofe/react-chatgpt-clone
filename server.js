6e3425fa9913776b1d85a56fd628bcf9a68d28f9
Bravo1B9 <brofe93@gmail.com>
2023-05-04 05:49:39 +0000

    Initial completions route setup

--- server.js
+++ server.js
@@ -7,6 +7,14 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+app.post('/completions', (req, res) => {
+  try {
+    res.json({ msg: 'This is the text completions route' });
+  } catch (error) {
+    console.log(error);
+  }
+});
+
 app.listen(PORT, () => {
   console.log(`App listening on port: ${PORT}`);
-})
+});
